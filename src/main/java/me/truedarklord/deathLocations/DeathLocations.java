d575735c50783adcb1b095cadd4990ca9eeb8857
TrueDarkLord <truedarklord.email@gmail.com>
2025-11-21 20:45:37 -0700

    Metrics
    Add BStats support.

--- src/main/java/me/truedarklord/deathLocations/DeathLocations.java
+++ src/main/java/me/truedarklord/deathLocations/DeathLocations.java
@@ -2,12 +2,15 @@ package me.truedarklord.deathLocations;
 
 import me.truedarklord.deathLocations.commands.Reload;
 import me.truedarklord.deathLocations.listeners.Death;
+import org.bstats.bukkit.Metrics;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class DeathLocations extends JavaPlugin {
 
     @Override
     public void onEnable() {
+        Metrics metrics = new Metrics(this, 28088);
+
         new Reload();
         new Death();
     }
